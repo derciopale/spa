fe9db6a1861d815f58d4748943232aff445e15f7
derciopale7 <derciopale7@gmail.com>
2022-02-02 14:57:43 +0200

    controller endereco

--- app/Http/Controllers/EnderecoController.php
+++ app/Http/Controllers/EnderecoController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Endereco;
 use Illuminate\Http\Request;
 
 class EnderecoController extends Controller
 {
-    //
+    public function index()
+    {
+        $enderecos = Endereco::all()->toArray();
+        return array_reverse($enderecos);
+    }
+
+    public function store(Request $request)
+    {
+        $endereco = new Endereco([
+            'provincia' => $request->input('provincia'),
+            'distrito' => $request->input('distrito'),
+            'bairro' => $request->input('bairro')
+        ]);
+        $endereco->save();
+
+        return response()->json('Endereco Criado!');
+    }
+
+    public function show($id)
+    {
+        $endereco = Endereco::find($id);
+        return response()->json($endereco);
+    }
+
+    public function update($id, Request $request)
+    {
+        $endereco = Endereco::find($id);
+        $endereco->update($request->all());
+
+        return response()->json('Endereco Actualizado!');
+    }
+
+    public function destroy($id)
+    {
+        $endereco = Endereco::find($id);
+        $endereco->delete();
+
+        return response()->json('Endereco Apagado!');
+    }
+
 }
