28156b043e2f62eec9e6bceacb4f5d659eaf2595
derciopale7 <derciopale7@gmail.com>
2022-02-02 14:36:57 +0200

    crud endereco

--- app/Models/Endereco.php
+++ app/Models/Endereco.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Endereco extends Model
 {
     use HasFactory;
+    protected $fillable = ['provincia', 'distrito', 'bairro'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function endereco()
+    {
+        return $this->hasOne(Endereco::class);
+    }
+
 }
